4f1e843793892ff7f8cba00e28ca3dde96749683
Winnie Happi <158840690+winniehappi1@users.noreply.github.com>
2025-10-22 12:45:50 -0400

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -12,6 +12,9 @@ WARNING:
     and ensure you have proper backups before running this script.
 */
 
+
+
+
 USE master;
 GO
 
@@ -38,88 +41,4 @@ CREATE SCHEMA silver;
 GO
 
 CREATE SCHEMA gold;
-GO
-CREATE TABLE gold.dim_customers(
-	customer_key int,
-	customer_id int,
-	customer_number nvarchar(50),
-	first_name nvarchar(50),
-	last_name nvarchar(50),
-	country nvarchar(50),
-	marital_status nvarchar(50),
-	gender nvarchar(50),
-	birthdate date,
-	create_date date
-);
-GO
-
-CREATE TABLE gold.dim_products(
-	product_key int ,
-	product_id int ,
-	product_number nvarchar(50) ,
-	product_name nvarchar(50) ,
-	category_id nvarchar(50) ,
-	category nvarchar(50) ,
-	subcategory nvarchar(50) ,
-	maintenance nvarchar(50) ,
-	cost int,
-	product_line nvarchar(50),
-	start_date date 
-);
-GO
-
-CREATE TABLE gold.fact_sales(
-	order_number nvarchar(50),
-	product_key int,
-	customer_key int,
-	order_date date,
-	shipping_date date,
-	due_date date,
-	sales_amount int,
-	quantity tinyint,
-	price int 
-);
-GO
-
---Load Customers
-TRUNCATE TABLE gold.dim_customers;
-GO
-
-BULK INSERT gold.dim_customers
-FROM '/var/opt/mssql/import/gold.dim_customers.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '0x0a',
-    TABLOCK
-);
-GO
-
-
---Load Products
-TRUNCATE TABLE gold.dim_products;
-GO
-
-BULK INSERT gold.dim_products
-FROM '/var/opt/mssql/import/gold.dim_products.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '0x0a',
-    TABLOCK
-);
-GO
-
---Load Sales
-TRUNCATE TABLE gold.fact_sales;
-GO
-
-BULK INSERT gold.fact_sales
-FROM '/var/opt/mssql/import/gold.fact_sales.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '0x0a',
-    TABLOCK
-);
 GO
